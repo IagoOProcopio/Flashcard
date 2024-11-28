1518ac13557024eb22836b3c0b03c474391f81de
iagoProgramador <00001078356476sp@al.educacao.sp.gov.br>
2024-11-28 18:02:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Marketing',
+    'o que é Neuromarketing?',
+    'o Neuromarketing é uma ferrament para usar a emoção do cliente',
+)
+
+criaCartao(
+    'Logística',
+    'O que é UEPS?',
+    'UEPS é uma linguagem para armazenamento onde o Último que Entra é o Primeiro que Sai',
+)
+
+criaCartao(
+    'Marketing',
+    'Qual cor gera a fome',
+    'É a amarela, já que suas cores são fortes',
+)
+
+criaCartao(
+    'Logística',
+    'O que é FOB?',
+    'FOB é um termo muito utilizado nos Incoterms',
+)
